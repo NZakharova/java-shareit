4f5c09de9be30d3370531b842d2a721404a7125e
A Zakharova <zac1303@yandex.ru>
2023-01-21 15:54:20 +0700

    Убрала неиспользуемые импорты из ItemRequestServiceTests

--- src/test/java/ru/practicum/shareit/requests/ItemRequestServiceTests.java
+++ src/test/java/ru/practicum/shareit/requests/ItemRequestServiceTests.java
@@ -1,12 +1,10 @@
 package ru.practicum.shareit.requests;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import ru.practicum.shareit.requests.dto.ItemRequestDto;

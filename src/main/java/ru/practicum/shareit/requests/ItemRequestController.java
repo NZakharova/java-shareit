0ef1fedf88ca4ab18cbe79d27baebbf0c7a62546
A Zakharova <zac1303@yandex.ru>
2023-01-24 19:40:45 +0700

    Добавила логирование в ItemRequestController

--- src/main/java/ru/practicum/shareit/requests/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/requests/ItemRequestController.java
@@ -18,22 +18,27 @@ public class ItemRequestController {
 
     @PostMapping("/requests")
     public ItemRequestDto createRequest(@RequestHeader("X-Sharer-User-Id") int userId, @Validated @RequestBody ItemRequestDto request) {
+        log.info("Создание запроса пользователем " + userId + ": " + request);
         int id = itemRequestService.add(userId, request);
+        log.info("Создан запрос " + id);
         return itemRequestService.get(id);
     }
 
     @GetMapping("/requests/all")
     public List<ItemRequestDto> getAllRequests(@RequestHeader("X-Sharer-User-Id") int userId, @RequestParam Optional<Integer> from, @RequestParam Optional<Integer> size) {
+        log.info("Получение всех запросов пользователем " + userId);
         return itemRequestService.getAll(userId, PaginationUtils.create(from, size));
     }
 
     @GetMapping("/requests/{id}")
     public ItemRequestDto getRequest(@RequestHeader("X-Sharer-User-Id") int userId, @PathVariable int id) {
+        log.info("Получение запроса " + id + " пользователем " + userId);
         return itemRequestService.get(userId, id);
     }
 
     @GetMapping("/requests")
     public List<ItemRequestDto> getRequests(@RequestHeader("X-Sharer-User-Id") int userId, @RequestParam Optional<Integer> from, @RequestParam Optional<Integer> size) {
+        log.info("Получение собственных запросов пользователем " + userId);
         return itemRequestService.getAllForUser(userId, PaginationUtils.create(from, size));
     }
 }

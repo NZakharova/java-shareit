4b8db7988ef428281bf6ebd40a980eefc677e41d
A Zakharova <zac1303@yandex.ru>
2023-02-09 22:45:06 +0700

    Проходят тесты для comments

--- gateway/src/main/java/ru/practicum/shareit/item/dto/CreateCommentRequest.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/CreateCommentRequest.java
@@ -2,14 +2,14 @@ package ru.practicum.shareit.item.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class CreateCommentRequest {
-    @NotNull
     @NotBlank
-    private final String text;
+    private String text;
 }
